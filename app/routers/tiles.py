2beb870523f74734de8785e2d24f24c9bf3b8c0e
Alp <alp.oktem@clearglobal.org>
2022-12-27 18:19:15 +0100

    audio playback, climate T1,2,3 content

--- app/routers/tiles.py
+++ app/routers/tiles.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 import requests
 import json
 import os
+import pygame
 from typing import Optional
 
 router = APIRouter()
@@ -12,27 +13,55 @@ templates = Jinja2Templates(directory="templates/")
 AUDIO_EXTS = ['.wav', '.WAV'] #Can be extended
 
 RHASSPY_URL = os.environ.get('RHASSPYURL') or "http://rhasspy:12101"
-RESPONSE_TSV_PATH = os.environ.get('RESPONSETSV') or 'data/rhasspy_responses_hin.tsv'
-print('Responses path:', RESPONSE_TSV_PATH)
+RESPONSE_TSV_PATH = os.environ.get('RESPONSETSV') or 'data/covid_hin.tsv'
+ANSWERS_AUDIO_PATH = os.environ.get('AUDIODIR') or 'data/audio'
+
+if os.path.exists(RESPONSE_TSV_PATH):
+    print('Responses path:', RESPONSE_TSV_PATH)
+else:
+    print("Responses file not found", RESPONSE_TSV_PATH)
+
+if os.path.exists(ANSWERS_AUDIO_PATH) and os.path.isdir(ANSWERS_AUDIO_PATH):
+    print('Audio responses path:', ANSWERS_AUDIO_PATH)
+else:
+    print("Audio responses path not found", ANSWERS_AUDIO_PATH)
 
 def say(text):
     url = RHASSPY_URL + "/api/text-to-speech"
     requests.post(url, text.encode('utf-8'))
+
+def play(audio):
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
 
 def read_response_data(tsv_path):
     import csv
     row_no = 1
     results = {}
     with open(tsv_path, newline='') as pscfile:
+        firstline = True
         for line in pscfile.readlines():
+            if firstline:
+                firstline = False
+                continue
             try:
-                key, val = line.strip().split('\t')
-                results[key] = val
+                line_info = line.strip().split('\t')
+                if len(line_info) == 2:
+                    intent_id, answer = line_info
+                    audio_path = None
+                elif len(line_info) == 3:
+                    intent_id, answer, audio_path = line_info
+
+                results[intent_id] = {'text': answer, 'audio': audio_path}
             except:
                 print("problem in row", row_no)
                 print(line)
             row_no += 1
         
+    print("Successfully read answers sheet. #intents:", len(results))
     return results
 
 response_data = read_response_data(RESPONSE_TSV_PATH)
@@ -51,12 +80,21 @@ def post_tiles(intentstr: str = Form(...)):
     print("intent", intent)
 
     response = response_data.get(intent)
-    print(response)
 
     if response:
         print("response", response)
-        say(response)
+        if response['audio']:
+            full_audio_path = os.path.abspath(os.path.join(ANSWERS_AUDIO_PATH, response['audio']))
+            if os.path.exists(full_audio_path):
+                print("play", full_audio_path)
+                play(full_audio_path)
+            else:
+                print(f"Couldn't file audio for intent {intent} in path {full_audio_path}. Using TTS instead")
+                say(response['text'])
+        else:
+            print("say", response['text'])
+            say(response['text'])
         return {"found":False, "id": ""}
     else:
-        print("ERROR: intent answer not specified")
+        print("Answer not specified for intent", intent)
 
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-export RESPONSETSV=data/rhasspy_responses_hin.tsv
+export RESPONSETSV=data/answers_climate_hin.tsv
 export RHASSPYURL="http://localhost:12101"
+export AUDIODIR="../TILES-audio"
 uvicorn app.main:app --reload --port 8080
