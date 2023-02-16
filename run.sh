c03b90bb066375d501f6ed54a09cec1916610adc
Alp <alp.oktem@clearglobal.org>
2023-02-16 18:06:50 +0100

    proto tested on local

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export RESPONSETSV=data/answers_climate_hin.tsv
 export RHASSPYURL="http://localhost:12101"
 export AUDIODIR="../TILES-audio"
-export FALLBACKAUDIOPATH=""
+export FALLBACKAUDIOPATH="data/GV_fallback_hin_climate.mp3"
 export SKIPAUDIOPLAY=1
 export TTSFALLBACK=0
 uvicorn app.main:app --reload --port 8080
