4ae9f240cc93bbfd9b1e7183ab231e4a91146bde
Alp <alp.oktem@clearglobal.org>
2023-03-16 10:39:21 +0100

    remove web app logs

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export AUDIODIR="../TILES-audio"
 export FALLBACKAUDIOPATH="data/GV_fallback_hin_climate.mp3"
 export SKIPAUDIOPLAY=1
 export TTSFALLBACK=0
-uvicorn app.main:app --reload --port 8080
+uvicorn app.main:app --reload --port 8080  --log-config logging.conf
